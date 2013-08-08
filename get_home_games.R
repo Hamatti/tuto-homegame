54b7ab3ff030d6e4341b960fe1bf781c4237d3a4
Juha-Matti Santala <jumasan@utu.fi>
2013-08-08 22:25:38 +0300

    Extra comments

--- get_home_games.R
+++ get_home_games.R
@@ -5,9 +5,12 @@ URL <- 'http://www.tutohockey.fi/fi/ottelut/otteluohjelma-2013-14'
 table <- as.data.frame(readHTMLTable(URL))
 names(table) <- c('date', 'time', 'place', 'home', 'away')
 
+# Drop the "extra info" column
 games <- table[-6]
+# Only consider regular season games
 games <- games[13:64,]
 
+# Only home games
 home_games <- games[games$home == 'TUTO Hockey',]
 
 write.csv(home_games, file="tutohomegames.csv")
